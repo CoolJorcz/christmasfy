96336799d13be214d21190251b8b1b021d97ac76
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-09-25 15:19:49 -0500

    Refactoring for use of images without src tags

--- application.js
+++ application.js
@@ -34,13 +34,13 @@
   var christmasGif = [
     new Christmas("square", "http://www.christmasgifs.org/artists/menard/full.gif"),
     new Christmas("vertical", "http://www.christmasgifs.org/artists/stockdale/full.gif"),
-    new Christmas("square", "http://www.christmasgifs.org/artists/dear/full.gif"),
+    new Christmas("horizontal", "http://www.christmasgifs.org/artists/dear/full.gif"),
     new Christmas("horizontal", "http://31.media.tumblr.com/bb9645bde39d7705ff13a5ffc4dd386c/tumblr_mfulpjEYoJ1rev1g5o1_500.gif"),
     new Christmas("horizontal", "http://24.media.tumblr.com/f05527c3cdaa1cdabbc7cc41e2cc9140/tumblr_mflqkpkQ8Q1r4uycao1_250.gif"),
-    new Christmas("vertical", "http://24.media.tumblr.com/623f269461eaa6c7ecf56ae3d74ee0d3/tumblr_mfiui7EfBq1qck7l0o1_r1_500.gif"),
-    new Christmas("square", "http://24.media.tumblr.com/8c19d8f533db5ebb433f9b9834b4c861/tumblr_mfgzzsWLIU1rmk138o1_400.gif"),
-    new Christmas("rectangle", "http://31.media.tumblr.com/425ae242c2b1c258c8de8d0802c5aa9e/tumblr_mfkqlcMpS51r5ge8lo8_250.gif"),
-    new Christmas("rectangle", "http://media3.giphy.com/media/4txbdOMP0CdP2/giphy.gif")
+    new Christmas("horizontal", "http://24.media.tumblr.com/623f269461eaa6c7ecf56ae3d74ee0d3/tumblr_mfiui7EfBq1qck7l0o1_r1_500.gif"),
+    new Christmas("horizontal", "http://24.media.tumblr.com/8c19d8f533db5ebb433f9b9834b4c861/tumblr_mfgzzsWLIU1rmk138o1_400.gif"),
+    new Christmas("horizontal", "http://31.media.tumblr.com/425ae242c2b1c258c8de8d0802c5aa9e/tumblr_mfkqlcMpS51r5ge8lo8_250.gif"),
+    new Christmas("horizontal", "http://media3.giphy.com/media/4txbdOMP0CdP2/giphy.gif")
   ]
 
   function imageRatio(image) {
@@ -58,32 +58,34 @@
 
   (function (document){
 
+    debugger
+    
     implementHoliday.init(christmasGif);
 
     var width;
     var height;
-    var images = document.getElementsByTagName('img');
-    
-    for (var i = 0; i < images.length; i ++) {
-      images[i].onload = function(){ 
+    var chrismages = document.getElementsByTagName('img')
+
+    imageCount = chrismages.length;
+    for (var i = 0; i < chrismages.length; i ++) {
+      chrismages[i].onload = function(){ 
         width = this.width;
         height = this.height;
-      }}
+    }}
       
-      imageCount = images.length;
 
       var image = document.getElementsByTagName('img')[0];
 
       for(var i = 0; i < imageCount; i++) {
-
-        var ratio = imageRatio(images[i]);
-        var number = Randomize(implementHoliday[ratio]);
-        var img = implementHoliday[ratio]()[number];
-        images[i].src = img.imageurl;
+        var image = chrismages[i]
+        if(image.attributes['src']) {
+          var ratio = imageRatio(image);
+          var number = Randomize(implementHoliday[ratio]);
+          var img = implementHoliday[ratio]()[number];
+          chrismages[i].src = img.imageurl;  
+        }
       }
-
       transformToChristmas();  
-
     })(document);
 
     function transformToChristmas(){
