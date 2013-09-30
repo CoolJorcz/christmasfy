2522e7d2069b9587e5ae4e2e47fcd64dd462be8c
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-09-30 17:03:47 -0500

    Continued whitespace thing

--- application.js
+++ application.js
@@ -40,12 +40,12 @@ var currentFlake = 0;
 var snowglobe = document.getElementById("snowglobe");
 
 while (currentFlake < flakeCount) {
-	var flake = document.createElement("a");
+	var flake = document.createElement("div");
 	flake.className = 'flake';
 	flake.style.fontSize = getRandom(12, 24) + 'px';
 	flake.style.top = getRandom(0, height) + 'px';
 	flake.style.left = getRandom(0, width) + 'px';
-	flake.innerHTML = "•"
+	flake.innerHTML = "*";
 	newFlake = snowglobe.appendChild(flake);
 	newFlake.speed = getRandom(1, 100);
 	flakes.push(newFlake);
